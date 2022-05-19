6cfc5edba0695f6a288b650b252e1e005377b82f
Ilan Malkin <malkin.ilan@gmail.com>
2022-05-19 21:39:13 +0200

    Fixed bug: gameRoutes no longer requires validateLogin

--- src/gameRoutes.js
+++ src/gameRoutes.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const path = require('path')
-const validateLogin = require('validateLogin')
 
 const express = require('express')
 const router = express.Router()
@@ -15,23 +14,6 @@ router.get('/singlePlayer', function (req, res) {
 
 router.get('/multiPlayer', function (req, res) {
   res.sendFile(path.join(__dirname, 'views', 'multiPlayer.html'))
-})
-
-router.post('/api/create', function (req, res) {
-  const validLogins = [
-    {
-      username: 'admin',
-      password: 'user'
-    },
-    {
-      username: 'someone',
-      password: 'password'
-    }
-  ]
-  if (validateLogin(req.username, req.password, validLogins)) {
-    console.log('Login successful')
-    res.redirect(req.baseUrl + '/views/multiplayer.html')
-  }
 })
 
 module.exports = router
