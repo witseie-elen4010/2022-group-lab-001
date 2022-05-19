15cccfbdaf35c667ecccb2ca281ce72ed53741e5
Gia Croock <2128541@students.wits.ac.za>
2022-05-19 19:40:30 +0200

    abstracted function

---
+++ src/__tests__/gameBoard.spec.js
@@ -0,0 +1,14 @@
+'use strict'
+/* eslint-env jest */
+
+/* const getCurrentPosition = require('./getCurrentPosition./public/index.js')
+describe('Check position of next free tile is correct', () => {
+  test('Add a letter', () => {
+    const currentRow = 0
+    const currentTile = 0
+    const position = getCurrentPosition(currentRow, currentTile)
+    expect(position.currentRow).toBe(0)
+    expect(position.currentTile).toBe(1)
+  })
+})
+*/
--- src/public/index.js
+++ src/public/index.js
@@ -1,3 +1,5 @@
+'use strict'
+// private
 const tileDisplay = document.querySelector('.tileContainer')
 const boardArray = [
   ['', '', '', '', ''],
@@ -10,32 +12,52 @@ const boardArray = [
 let currentRow = 0
 let currentTile = 0
 
-boardArray.forEach((boardRow, boardRowIndex) => {
-  const rowElement = document.createElement('div')
-  rowElement.setAttribute('id', 'boardRow-' + boardRowIndex)
-  boardRow.forEach((tile, tileIndex) => {
-    const tileElement = document.createElement('div')
-    tileElement.setAttribute('id', 'boardRow-' + boardRowIndex + '-tile-' + tileIndex)
-    tileElement.classList.add('tile')
-    rowElement.append(tileElement)
+// public
+
+function generateBoard () {
+  // Loop through each row and each tile to create the board
+  boardArray.forEach((boardRow, boardRowIndex) => {
+    const rowElement = document.createElement('div')
+    rowElement.setAttribute('id', 'boardRow-' + boardRowIndex)
+    boardRow.forEach((tile, tileIndex) => {
+      const tileElement = document.createElement('div')
+      tileElement.setAttribute('id', 'boardRow-' + boardRowIndex + '-tile-' + tileIndex)
+      tileElement.classList.add('tile')
+      rowElement.append(tileElement)
+    })
+    tileDisplay.append(rowElement)
   })
-  tileDisplay.append(rowElement)
-})
+}
+function getCurrentPosition (previousRow, previousTile) {
+  // for future functionality this must deal with the logic for deleting and element and for moving to the next row
+  previousTile++
+  return { previousRow, previousTile }
+}
 
-// letter input from keyboard, later should be updated to work with onc screen keyboard
-document.addEventListener('keypress', (event) => {
-  const letter = event.key
-  console.log('this is letter', letter)
-  addLetter(letter)
-})
-
-const addLetter = (letter) => {
+function addLetter (letter) {
+  const previousRow = currentRow
+  const previousTile = currentTile
   // to ensure we only enter 5 letters in one row
   if (currentTile < 5 && currentRow < 6) {
     const tile = document.getElementById('boardRow-' + currentRow + '-tile-' + currentTile)
     tile.textContent = letter
     boardArray[currentRow][currentTile] = letter
     console.log('boardRow', boardArray)
-    currentTile++
+    const position = getCurrentPosition(previousRow, previousTile)
+
+    currentRow = position.previousRow
+    currentTile = position.previousTile
+    console.log('position', position[1])
+    // currentTile++
   }
 }
+
+generateBoard()
+
+// letter input from keyboard, later should be updated to work with on screen keyboard-just used to visually check its working
+document.addEventListener('keypress', (event) => {
+  const letter = event.key
+  console.log('this is letter', letter)
+  addLetter(letter)
+})
+module.exports = getCurrentPosition
