6bb92f38b60a99d8adab548692ac6e788c2f51d0
Ilan Malkin <malkin.ilan@gmail.com>
2022-05-24 13:31:33 +0200

    Added: backend validation for signup

--- src/__tests__/validateLogin.test.js
+++ src/__tests__/validateLogin.test.js
@@ -1,6 +1,9 @@
 /* eslint-env jest */
-const validate = require('../validateLogin')
+const loginServer = require('../loginServer')
+const login = loginServer.login
+const signup = loginServer.signup
 
+// test login function
 const validLogins = [
   {
     username: 'admin',
@@ -13,19 +16,46 @@ const validLogins = [
 ]
 
 test('Valid logins', () => {
-  expect(validate('admin', 'user', validLogins)).toBe(true)
-  expect(validate('someone', 'password', validLogins)).toBe(true)
+  expect(login('admin', 'user', validLogins)).toBe(true)
+  expect(login('someone', 'password', validLogins)).toBe(true)
 })
 
 test('Invalid logins', () => {
   // not contained at all
-  expect(validate('john', 'secret', validLogins)).toBe(false)
+  expect(login('john', 'secret', validLogins)).toBe(false)
   // valid username, invalid password
-  expect(validate('admin', 'invalidpassword', validLogins)).toBe(false)
+  expect(login('admin', 'invalidpassword', validLogins)).toBe(false)
   // valid password of some user, invalid username
-  expect(validate('john', 'user', validLogins)).toBe(false)
+  expect(login('john', 'user', validLogins)).toBe(false)
   // username of one user, password of another
-  expect(validate('admin', 'password', validLogins)).toBe(false)
+  expect(login('admin', 'password', validLogins)).toBe(false)
   // empty password
-  expect(validate('admin', '', validLogins)).toBe(false)
+  expect(login('admin', '', validLogins)).toBe(false)
+})
+
+// test signup function
+// happy case
+test('Valid singup', () => {
+  expect(signup('email@example.com', 'letterpassword', 'letterpassword')).toBe(true)
+  expect(signup('1234567890@example.com', '!kdjf#kdfj89', '!kdjf#kdfj89')).toBe(true)
+})
+
+test('Invalid email', () => {
+  expect(signup('plainaddress', '!kdjf#kdfj89', '!kdjf#kdfj89')).toBe(false)
+  expect(signup('Joe Smith <email@example.com>', '!kdjf#kdfj89', '!kdjf#kdfj89')).toBe(false)
+  expect(signup('email@111.222.333.44444', '!kdjf#kdfj89', '!kdjf#kdfj89')).toBe(false)
+})
+
+test('Password less than 8 characters is invalid', () => {
+  expect(signup('email@example.com', 'short', 'short')).toBe(false)
+  expect(signup('email@example.com', '7letter', '7letter')).toBe(false)
+})
+
+test('Non-matching passwords are invalid', () => {
+  expect(signup('email@example.com', 'letterpassword', 'Letterpassword')).toBe(false)
+  expect(signup('email@example.com', '!kdjf#kdfj89', '!kdjf#kdfj891')).toBe(false)
+})
+
+test('Email must not match password', () => {
+  expect(signup('email@example.com', 'email@example.com', 'email@example.com')).toBe(false)
 })
--- src/loginServer.js
+++ src/loginServer.js
@@ -1,4 +1,11 @@
-function validateLogin (username, password, validLogins) {
+// email validation front-end function from W3docs
+// source: https://www.w3docs.com/snippets/javascript/how-to-validate-an-e-mail-using-javascript.html
+function validateEmail (email) {
+  const res = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return res.test(String(email).toLowerCase())
+}
+
+function login (username, password, validLogins) {
   let found = false
   validLogins.every(validLogin => {
     if (username === validLogin.username && password === validLogin.password) {
@@ -17,4 +24,25 @@ function validateLogin (username, password, validLogins) {
   }
 }
 
-module.exports = validateLogin
+function signup (email, password, confirmPassword) {
+  // validation
+  if (!validateEmail(email)) {
+    return false
+  }
+  if (password.length < 8) {
+    return false
+  }
+  if (password === email) {
+    return false
+  }
+  if (password !== confirmPassword) {
+    return false
+  }
+
+  // happy case
+  console.log(`Successfully logged in as ${email}`)
+  return true
+}
+
+module.exports.login = login
+module.exports.signup = signup
