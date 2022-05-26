b1953900322de81d2cf5fc6196803ab74be9845c
Ilan Malkin <malkin.ilan@gmail.com>
2022-05-26 19:15:35 +0200

    Removed old commented code from login.js in response to code review.

--- src/public/scripts/login.js
+++ src/public/scripts/login.js
@@ -1,22 +1,3 @@
-// function validateLogin (username, password, validLogins) {
-//   let found = false
-//   validLogins.every(validLogin => {
-//     if (username === validLogin.username && password === validLogin.password) {
-//       found = true
-//       // break out of loop
-//       return false
-//     }
-//     // if login doesn't match, iterate
-//     return true
-//   })
-//   if (found) {
-//     // login successful
-//     return true
-//   } else {
-//     return false
-//   }
-// }
-
 const loginForm = document.getElementById('login-form')
 const loginButton = document.getElementById('login-form-submit')
 
@@ -26,14 +7,6 @@ loginButton.addEventListener('click', e => {
     email: loginForm.username.value,
     password: loginForm.password.value
   }
-  // // hard coded temporarily
-
-  // const validLogins = [
-  //   {
-  //     username: 'admin',
-  //     password: 'user'
-  //   }
-  // ]
 
   // request options
   const options = {
@@ -50,30 +23,5 @@ loginButton.addEventListener('click', e => {
       if (response.ok) {
         window.location.href = '../views/index'
       }
-      // else {
-      //   window.alert('OOPS')
-      // }
     })
-
-  // .then(message => window.alert(message))
-  // .catch(err => window.alert(err))
-  // if (response.ok) {
-  //   console.log('Response ok')
-  //   window.alert('Login successful')
-  // } else {
-  //   window.alert(response.message)
-  // }
-  // })
-
-  // .catch(error => {
-  //   console.log('Something went wrong.', error)
-  // })
-  // if (validateLogin(username, password, validLogins)) {
-  //   window.alert('Login succesful')
-  //   console.log('Login succesful')
-  //   window.location.href = '../views/index'
-  // } else {
-  //   window.alert('Invalid username or password')
-  //   console.log('Invalid username or password')
-  // }
 }, false)
