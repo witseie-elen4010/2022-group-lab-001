361535a48b1baa5b7dde360177cfb89705d2eba0
Gia Croock <2128541@students.wits.ac.za>
2022-05-26 10:46:54 +0200

    testing added

--- src/__tests__/words.spec.js
+++ src/__tests__/words.spec.js
@@ -1,30 +1,35 @@
 /* eslint-env jest */
-// const request = require('supertest')
-const wordController = require('../controllers/wordController')
-// const express = require('express')
-// const app = express()
-// const bodyParser = require('body-parser')
+const request = require("supertest")
+const wordController = require("../controllers/wordController")
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
 
-// const router = require('../routes/wordRoutes')
-// app.use(bodyParser.json())
-// app.use(bodyParser.urlencoded({ extended: true }))
-// app.use('/', router)
+const router = require("../routes/wordRoutes");
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use("/", router)
 
-const dictionary = require('../models/dictionary')
-const { options } = require('..')
-const validWords = dictionary.getDictionary()
+const dictionary = require("../models/dictionary")
+const validWords = dictionary.getDictionary();
 
-/*describe('When making a request to /word', () => {
-  it('should return a valid word', async () => {
-    const response = await request(app).get('/')
-    word = response.body
-    expect(response.statusCode).toBe(200)
+describe('When making a request to /word', () => {
+  it('should return a valid word for the day', async () => {
+    const word=wordController.getWordOfTheDay()
     expect(word.length).toBe(5)
     expect(validWords.includes(word)).toBe(true)
   })
-})*/
+})
 
-test('Tests whether a new index is generated per day', () => {
+test("Testing post request to check if a word is equal to the word of the day the response is correct", async () => {
+  const wordOfTheDay = wordController.getWordOfTheDay()
+  const body = { guess: wordOfTheDay };
+  const response = await request(app).post("/").send(body)
+  expect(response.statusCode).toBe(200)
+  expect(response.body).toBe('word of the day')
+})
+
+test("Tests whether a new index is generated per day", () => {
   const date1 = new Date()
   const date2 = new Date()
   date1.setFullYear(2022, 4, 2)
@@ -32,4 +37,4 @@ test('Tests whether a new index is generated per day', () => {
   index1 = wordController.getRandomIndexBasedOnDate(date1)
   index2 = wordController.getRandomIndexBasedOnDate(date2)
   expect(index1).not.toBe(index2)
-}) 
+})
